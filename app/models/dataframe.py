b45e28683b2d81820ce455bb16d36c5720dfb519
ekrsw <eisuke.koresawa@gmail.com>
2023-11-07 22:18:15 +0900

    commit

--- app/models/dataframe.py
+++ app/models/dataframe.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import numpy as np
 import os
 
 import pandas as pd
@@ -51,6 +52,7 @@ class ReporterDataFrame(BaseDataFrame):
         Args:
             close_file(str): クローズデータのファイル名
             date_obj(dt.date): 日付
+            
         return:
             df(pd.DataFrame): 指定日のクローズデータ"""
 
@@ -80,23 +82,57 @@ class ReporterDataFrame(BaseDataFrame):
 
         return df
         
-    def __init__(self, df, closefile, from_date, to_date, *args, **kwargs):
+    def __init__(self, df, closefile, from_date, to_date, additon, *args, **kwargs):
         super().__init__(df, *args, **kwargs)
         self.from_date = from_date
         self.to_date = to_date
+        self.addition = additon
         self._convert_reporter_names()
         self.rename_axis('氏名', inplace=True)
         close_df = self._read_close_file(closefile, from_date, to_date)
         join_df = self.join(close_df, how='outer').fillna(0)
         self.update_self(join_df)
+        self.loc['合計'] = self.sum()
+        added_df = self._create_acw_att_cph_columns()
+        self.update_self(added_df)
     
     def test(self):
         print(self[['ワークタイムの合計', 'クローズ']])
 
+    def _create_acw_att_cph_columns(self):
+        '''スクレイピングした指標から、ACW, ATT, CPHを計算してselfにカラムを追加して返す
+        0除算を避けるために、0の場合はいったん1にreplaceしている'''
+
+        # columns_to_convert = ['着信通話時間の合計','発信通話時間の合計', 'ワークタイムの合計', '着信後処理時間の合計', '発信後処理時間の合計', '離席時間の合計', '事前準備時間の合計', '一時離席時間の合計', '転送可時間の合計', '着信通話時間の合計(外線)', '発信通話時間の合計(外線)', 'クローズ']
+
+        # 計算に使用するカラムの値をfloatに変換
+        # for col in columns_to_convert:
+        #    self[col] = self[col].astype(float)
+        
+        # 実際の計算
+        self['ACW'] = (self['ワークタイムの合計'] + self['着信後処理時間の合計'] + self['発信後処理時間の合計'] + self['事前準備時間の合計'] + self['転送可時間の合計'] + self['一時離席時間の合計']) / self['クローズ'].replace(0, 1)
+        self.loc[self['クローズ']==0, 'ACW'] = 0
+        self['ATT'] = (self['着信通話時間の合計(外線)'] + self['発信通話時間の合計(外線)']) / self['クローズ'].replace(0, 1)
+        self.loc[self['クローズ']==0, 'ATT'] = 0
+        
+        if self.addition:
+            _tmp = (self['着信通話時間の合計'] + self['発信通話時間の合計'] + self['ワークタイムの合計'] + self['着信後処理時間の合計'] + self['発信後処理時間の合計'] + self['離席時間の合計'] + self['事前準備時間の合計'] + self['一時離席時間の合計']) * 24
+        else:
+            _tmp = (self['ログオン時間'] - (self['待機時間'] + self['昼休憩時間の合計'] + self['研修/会議時間の合計'] + self['別作業中時間の合計'] + self['他者支援時間の合計'] + self['開発資料確認時間の合計'] + self['資料作成時間の合計'])) * 24
+        self['CPH'] = np.where(
+            _tmp == 0,
+            0,
+            self['クローズ'] / _tmp
+        )
+        
+        self = self.replace(np.inf, 0)
+
+        return self
+
 
 class ActivityDataFrame(BaseDataFrame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, df, *args, **kwargs):
+        super().__init__(df, *args, **kwargs)
 
          # メンバーリストを読込み、'氏名'、'グループ'のカラムのみにする。
         df_member_group = self._load_member_list()
@@ -126,9 +162,33 @@ class ActivityDataFrame(BaseDataFrame):
         self.reset_index(drop=True, inplace=True)
 
 def read_reporter(close_file, from_date, to_date) -> ReporterDataFrame:
+    """指定した範囲のReporterDataFrameを作成する。
+    
+    Args:
+        close_file(str): クローズデータのファイル名
+        from_date(date): 集計期間のfrom
+        to_date(date): 集計期間のto
+    
+    return:
+        df(ReporterDataFrame): テーブルをReporterDataFrameに変換し、'クローズ'カラムを追加したもの。
+    """
     reporter = Reporter(headless_mode=settings.HEADLESS_MODE)
     df = reporter.get_table_as_dataframe(settings.REPORTER_TEMPLATE, from_date, to_date)
-    return ReporterDataFrame(df, close_file, from_date, to_date)
+    return ReporterDataFrame(df, close_file, from_date, to_date, False)
+
+def read_todays_reporter(close_file) -> ReporterDataFrame:
+    """本日のReporterDataFrameを作成する。
+    
+    Args:
+        close_file(str): クローズデータのファイル名
+    
+    return:
+        df(ReporterDataFrame): テーブルをReporterDataFrameに変換し、'クローズ'カラムを追加したもの。
+    """
+    date_obj = dt.date.today()
+    reporter = Reporter(headless_mode=settings.HEADLESS_MODE)
+    df = reporter.get_table_as_dataframe(settings.REPORTER_TEMPLATE, date_obj, date_obj)
+    return ReporterDataFrame(df, close_file, date_obj, date_obj, True)
 
 def read_activity(filename, date_obj, *args, **kwargs) -> ActivityDataFrame:
     df = pd.read_excel(filename, *args, **kwargs)
--- test.py
+++ test.py
@@ -11,7 +11,8 @@ import settings
 
 close_file = os.path.join(settings.FILES_PATH, settings.TODAYS_CLOSE_FILE)  
 
-df = adf.read_reporter(close_file, dt.date.today(), dt.date.today())
+# df = adf.read_reporter(close_file, dt.date.today(), dt.date.today())
 # df_2 = adf.read_activity(r"\\mjs.co.jp\datas\CSC共有フォルダ\第47期 東京CSC第二グループ\47期SV共有\ダッシュボードアイテム\files\activity_files\202310_activity.xlsx", dt.date(2023, 10, 15))
+df = adf.read_todays_reporter(os.path.join(settings.FILES_PATH, settings.TODAYS_CLOSE_FILE))
 
-df.test()
+print(df)
