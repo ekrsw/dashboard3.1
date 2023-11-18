72f05a1bd028ad95332441b82347de1d3a444db4
ekrsw <eisuke.koresawa@gmail.com>
2023-11-18 15:29:16 +0900

    commit

--- test2.py
+++ test2.py
@@ -8,7 +8,7 @@ import app.models.dataframe as adf
 import settings
 
 
-date_obj = dt.date.today()
+date_obj = dt.date(2023, 11, 17)
 
 from_date = date_obj
 to_date = date_obj
@@ -17,7 +17,7 @@ date_str = from_date.strftime('%Y%m')
 file_name = f'{date_str}_activity.xlsx'
 
 # activity_file = os.path.join(settings.FILES_PATH, 'todays_activity.xlsm')
-activity_file = os.path.join(r'\\mjs.co.jp\datas\CSC共有フォルダ\第47期 東京CSC第二グループ\47期SV共有\ダッシュボードアイテム\files\activity_files\比較用', '202311_activity.xlsx')
+activity_file = os.path.join(r'\\mjs.co.jp\datas\CSC共有フォルダ\第47期 東京CSC第二グループ\47期SV共有\ダッシュボードアイテム\files\activity_files', '202311_activity.xlsx')
 
 df = adf.read_activity(activity_file, from_date, to_date)
 pending_df = adf.read_pending_case(activity_file)
