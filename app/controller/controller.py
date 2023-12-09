ed309bba446d46b024630d00ecbfa6d0b4f4cf9d
ekrsw <eisuke.koresawa@gmail.com>
2023-12-09 13:26:52 +0900

    週休

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -35,6 +35,9 @@ def df_to_html():
     dep_att = df_performance.loc['合計', 'ATT']
     dep_cph = df_performance.loc['合計', 'CPH']
     
+    # シフトが'週休'となっているレコードを削除する。
+    df_performance = df_performance[df_performance['シフト'] != '週休']
+
     # KPIの件数のDataFrameから、直受け率、20分以内率のDataFrameを作成する。
     df_kpi_ratio = mp.calc_ratio_20_40(df_kpi_count)
 
